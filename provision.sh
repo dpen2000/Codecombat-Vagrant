e813cdda9365e6e7f29b48ede5d2fa4c8221382d
dpen2000 <davidpendray@gmail.com>
2014-05-17 23:07:07 +0100

    Closes #4 by installing ruby and sass as part of provision

--- provision.sh
+++ provision.sh
@@ -13,4 +13,6 @@ sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
 sudo apt-get -y update
 sudo apt-get -y install mongodb-org
+sudo apt-get -y install ruby1.9.1 ruby1.9.1-dev
+sudo gem install sass
 npm install mongoose
