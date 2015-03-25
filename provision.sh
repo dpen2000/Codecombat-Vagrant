46ca6cc82e680109381f5e9f20dc7e5ee38cbffa
Alan Hohn <Alan.M.Hohn@lmco.com>
2015-03-25 19:11:11 -0400

    install geoip-lite npm module

--- provision.sh
+++ provision.sh
@@ -9,6 +9,7 @@ cd /coco
 sudo npm install
 sudo npm install -g bower
 sudo npm install -g brunch
+sudo npm install -g geoip-lite
 bower install --allow-root
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
